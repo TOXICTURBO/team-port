7e2749909d2dd6e66715953b9cc479894d8cafe6
Toxic-Turbo <sanjai1881991@gmail.com>
2024-09-08 18:26:14 +0530

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,12 +17,10 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your</span> digital
-          presence to the next level?
+        <span className="text-purple">RIS</span> Esports
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          Uniting passion and precision in Free Fire.
         </p>
         <a href="mailto:contact@jsmastery.pro">
           <MagicButton
@@ -34,7 +32,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
+          Copyright © 2024 RIS ESPORTS
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
