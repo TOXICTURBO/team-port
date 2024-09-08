29ec71e941e19a3b687f01ee26e6cd0df05f802f
Toxic-Turbo <sanjai1881991@gmail.com>
2024-09-08 21:33:08 +0530

    fix

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/MovingBorders";
 
 const Experience = () => {
   return (
-    <div className="py-20 w-full" id="players">>
+    <div className="py-20 w-full">
       <h1 className="heading">
         <span className="text-purple">PLAYERS</span>
       </h1>
